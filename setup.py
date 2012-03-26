53b2ce620c9a4749cf6886b9e7372798c99544ab
Justin Fenn <justin@factual.com>
2012-03-26 02:10:14 -0700

    Added some information to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ required = ['requests-oauth >= 0.3.0']
 setup(
     name='factual-api',
     version='0.1',
-    packages=find_packages(),
-    url='https://github.com/Factual/factual-python-driver',
     description='Official Python driver for the Factual public API',
+    url='http://github.com/Factual/factual-python-driver',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    keywords=['factual'],
+    packages=find_packages(),
     install_requires=required
 )
