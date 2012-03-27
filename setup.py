a5a3aa9f23c726c72cb9d5267e47d3c8986a3604
Justin Fenn <justin@factual.com>
2012-03-27 02:03:54 -0700

    A few more changes to get ready for release

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup, find_packages
 required = ['requests-oauth >= 0.3.0']
 
 setup(
+    author='Factual Driver Team',
     name='factual-api',
-    version='0.1',
+    version='0.9.0',
     description='Official Python driver for the Factual public API',
+    long_description=open('README.md').read(),
     url='http://github.com/Factual/factual-python-driver',
+    license='Apache License',
     classifiers=(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
